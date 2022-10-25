7c0fab7c3026662d2b1c1613ab9482bcb62a9a14
MilanaNev <116630670+MilanaNev@users.noreply.github.com>
2022-10-25 11:31:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let phrases = [
-  { text: 'отправить другу смешную гифку', image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif' },
-  { text: 'посмотреть скидки на авиабилеты', image: 'https://code.s3.yandex.net/web-code/procrastinate/2.png' },
+  { text: 'понять физику ', image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif' },
+  { text: 'сходить на концерт', image: 'https://code.s3.yandex.net/web-code/procrastinate/2.png' },
   { text: 'разобраться, о чём поют рэперы', image: 'https://code.s3.yandex.net/web-code/procrastinate/3.png' },
   { text: 'Юрий Дудь', image: 'https://code.s3.yandex.net/web-code/procrastinate/4.png' },
-  { text: 'расставить книги на полке по цвету', image: 'https://code.s3.yandex.net/web-code/procrastinate/5.png' },
+  { text: 'смотреть сериал кухня в 100 раз', image: 'https://code.s3.yandex.net/web-code/procrastinate/5.png' },
   { text: 'читать про зарплаты в Сан-Франциско', image: 'https://code.s3.yandex.net/web-code/procrastinate/6.png' },
   { text: 'прочитать новости и ужаснуться в комментариях', image: 'https://code.s3.yandex.net/web-code/procrastinate/7.png' },
   { text: 'попасть в поток грустных песен и вспомнить все ошибки молодости', image: 'https://code.s3.yandex.net/web-code/procrastinate/8.png' },
